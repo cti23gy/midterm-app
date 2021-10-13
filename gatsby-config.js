216e168ca60aa62cf26c60fb41d5cc86626a4ea9
cti23gy <70219724+cti23gy@users.noreply.github.com>
2021-10-13 15:18:41 -0400

    tests

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~ctimel/N322/midterm-app",
     title: "midterm-app",
   },
-  plugins: [],
+  plugins: [
+    {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      short_name: "midterm-app",
+      display: "standalone",
+      icon: 'src/images/icon.png',
+    }
+  },
+  `gatsby-plugin-offline`
+],
+pathPrefix: '/~ctimel/N322/midterm-app'
 };
